dae51ba71f12bfac2826c5ce7fc8013fe066a9a7
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-02-01 01:18:48 +0200

    Add files via upload

---
+++ Листинг 7.2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using static System.Console;
+
+namespace Листинг_7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (char s in "Текст")
+            {
+                Write(s + " ");
+            }
+            WriteLine();
+
+            for (int i = 0; i < "Тескт".Length; i++)
+            {
+                Write("Текст"[i] + "_");
+            }
+            WriteLine();
+
+            String A = "Изучаваме C#";
+            for (int i = 0; i < A.Length; i++)
+            {
+                Write(A[i]);
+            }
+            WriteLine();
+
+            showReversed(A);
+            WriteLine(A);
+            String B = getReversed(A);
+            WriteLine(B);
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+        static String getReversed(String text)
+        {
+            String str = "";
+            foreach (char s in text)
+            {
+                str = s + str;
+            }
+            return str;
+        }
+        static void showReversed(String text)
+        {
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                Write(text[i]);
+            }
+            WriteLine();
+        }
+    }
+}
---
+++ Листинг 7.3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+
+namespace Листинг_7._3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static bool StrCmp(String X, String Y)
+            {
+                if (X.Length != Y.Length) return false;
+                for (int i = 0; i < X.Length; i++)
+                {
+                    if (X[i] != Y[i]) return false;
+                }
+                return true;
+            }
+
+            char[] smb = { 'Е', 'з', 'и', 'к', ' ', 'C', '#' };
+            String A = "Език C#";
+            WriteLine("A: \"{0}\"", A);
+
+            String B = new String(smb);
+            WriteLine("B: \"{0}\"", B);
+
+            String C = "ЕЗИК C#";
+            WriteLine("C: \"{0}\"", C);
+
+            WriteLine("Сравняване на низове");
+            WriteLine("A==B: {0}", A == B);
+            WriteLine("StrCmp(A,B): {0}", StrCmp(A, B));
+
+            WriteLine("A==C: {0}", A == C);
+            WriteLine("StrCmp(A,C): {0}", StrCmp(A, C));
+
+            WriteLine("B!=C: {0}", B != C);
+            WriteLine("StrCmp(A,\"C#\"): {0}", StrCmp(A, "C#"));
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
