c34ba281ae3e7b282316973708bd9ed5a7b195ec
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-02-02 15:41:22 +0200

    Update Program.cs

--- Листинг 7.1/Program.cs
+++ Листинг 7.1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using static System.Console;
 
 namespace Листинг_7._1
@@ -7,31 +7,39 @@ namespace Листинг_7._1
     {
         static void Main(string[] args)
         {
+            //defining a charater array with several elements and we display it on the console
             char[] symbs = { 'Е', 'З', 'И', 'К', ' ', 'C', '#' };
             WriteLine("Simvolen masilv: ");
             WriteLine(symbs);
 
+            //displaying the array in quotes
             String A = new String(symbs);
             WriteLine("A: \"{0}\"", A);
 
-            String B = new String(symbs, 1, 5);
+            //displaying the array in quotes beginning from the second element
+            String B = new String(symbs, 1, 6);
             WriteLine("B: \"{0}\"", B);
 
-            String C = new String('i', 7);
+            //displaying one symbol seven times;
+            String C = new String('И', 7);
             WriteLine("C: \"{0}\"", C);
 
+            //assigning the function ((getText) with 2 references) to A and displaying it
             A = getText('A', 'H');
             WriteLine("A: \"{0}\"", A);
 
+            //assinging the function ((getText) with 2 references) to B and displaying it
             B = getText('H', 'A');
             WriteLine("B: \"{0}\"", B);
 
+            //assigning the function ((getText) with 1 reference) to C and displaying
             C = getText(symbs);
             WriteLine("C: \"{0}\"", C);
 
             WriteLine("Press any key to close...");
             ReadKey(true);
         }
+        //displays the array
         static String getText(char[] symbs)
         {
             String text = " ";
@@ -41,9 +49,10 @@ namespace Листинг_7._1
             }
             return text;
         }
+        //displays the charaters between the symbols
         static String getText(char a, char b)
         {
-            String text = " ";
+            String text = "";
             char s = a;
             while (s <= b)
             {
