8581aeb57016d9863fcc92f47c078bcf22466025
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-02-04 21:46:22 +0200

    Add files via upload

---
+++ Самостоятелни задачи/Задача 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_3
+{
+    class Program
+    {
+        static bool Meth(string a, string b)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i]) return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            String a = "The quick brown fox jumps over the lazy dog.";
+            String b = "The quick brown fox jumps over the lazy dog.";
+            String c = "The quick brown fox jumps over the lazy cat.";
+
+            WriteLine("a = b: {0}", Meth(a, b));
+            WriteLine("a = c: {0}", Meth(a, c));
+            WriteLine("b = c: {0}", Meth(b, c));
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
---
+++ Самостоятелни задачи/Задача 4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_4
+{
+    class Program
+    {
+        static bool Meth(string a, string b){
+            if (a.Length != b.Length) return false;
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            String a = "This sentence is 5 words long";
+            String b = "That one is not 67 words long";
+            String c = "This one is 5 words long too";
+            WriteLine("a = b: {0}", Meth(a, b));
+            WriteLine("a = c: {0}", Meth(a, c));
+            WriteLine("b = c: {0}", Meth(b, c));
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
