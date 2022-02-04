6bb07a8056edc0a88c856c56c4015ece661b37f5
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-02-04 21:48:10 +0200

    Add files via upload

---
+++ Самостоятелни задачи/Задача 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_5
+{
+    class Program
+    {
+        static void Meth(String a, char b)
+        {
+            int k = 0;
+            int j = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == b) { k++; }
+            }
+            int[] nums = new int[k];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = a.IndexOf(b, j + 1);
+                j = nums[i];
+                Write(nums[i] + "\n");
+            }
+            static void Main(string[] args)
+            {
+                String txt = "A seal is on the beach.";
+                char a = 'e';
+                Meth(txt, a);
+
+                WriteLine("Press any key to close...");
+                ReadKey(true);
+            }
+        }
+    }
+}
---
+++ Самостоятелни задачи/Задача 6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_6
+{
+    class Program
+    {
+        static void Meth(string a){
+            String s = a;
+            s = s.Replace("'","");
+            s = s.Replace(" ","");
+            s = s.Replace(".","");
+            s = s.Replace("?","");
+            s = s.Replace(",","");
+            s = s.Replace("!","");
+            s = s.Replace("?","");
+            s = s.ToLower();
+            char[] letters = s.ToCharArray();
+            Array.Sort(letters);
+            for(int i = 0; i < letters.Length; i++){
+                Write(letters[i]+"");
+            }
+            WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            String text = "The quick brown fox jumps over the lazy dog.'?!,";
+            Meth(text);
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
