1cbff54cf5b4f4b1d0db736c417a3e6c0824f93e
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-02-04 18:00:24 +0200

    Add files via upload

---
+++ Самостоятелни задачи/Задача 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_1
+{
+    class Program
+    {
+        static void Meth(string txt)
+        {
+            char[] t = txt.ToCharArray();
+            for (int i = 0; i < t.Length; i++)
+            {
+                Write(t[i] + " ");
+            }
+            WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            String txt = "The quick brown fox jumped over the lazy dog.";
+            Meth(txt);
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
---
+++ Самостоятелни задачи/Задача 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_2
+{
+    class Program
+    {
+        static string Meth(string txt)
+        {
+            string text = "";
+            for (int i = 0; i < txt.Length; i++)
+            {
+                text = txt[i] + text;
+            }
+            return text;
+        }
+        static void Main(string[] args)
+        {
+            String txt = "The quick brown fox jumps over the lazy dog.";
+            WriteLine("Оригиналното изречение:\n" + txt);
+            WriteLine("Изречението в обратен реди:\n" + Meth(txt));
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
